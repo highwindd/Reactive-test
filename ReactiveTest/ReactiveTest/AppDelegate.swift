b6ccc8a0fae1c17d493409801ff4200c125351c4
Kasianenko Boris <kasianenko.boris@yandex.ru>
2017-03-04 11:18:07 +0200

    Initial project commit.

---
+++ ReactiveTest/ReactiveTest/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ReactiveTest
+//
+//  Created by Kasianenko Boris on 04.03.17.
+//  Copyright © 2017 Kasianenko Boris. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ ReactiveTest/ReactiveTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ReactiveTest
+//
+//  Created by Kasianenko Boris on 04.03.17.
+//  Copyright © 2017 Kasianenko Boris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+}
+
